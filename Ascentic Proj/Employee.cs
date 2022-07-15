a7ff8b12cdcaa28c46d81ef0b626d05c0fab873e
DilshanAscentic <108863944+DilshanAscentic@users.noreply.github.com>
2022-07-15 08:33:47 +0530

    Update Employee.cs

--- Ascentic Proj/Employee.cs
+++ Ascentic Proj/Employee.cs
@@ -21,7 +21,9 @@ namespace Ascentic_Proj
         public double otHours { get; set; }
 
         public double otPrecent { get; set; } 
-
+        
+        public double otPrecentt { get; set; } 
+        
         public double overtime { get; set; }
 
         public double basicSalPrec { get; set; }
